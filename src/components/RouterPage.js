6642443693cd08ee22f05cac627145264457f2fe
Sajjad khan <shaankhan178@gmail.com>
2020-05-01 20:09:28 +0530

    added side menu and header

---
+++ src/components/RouterPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./header/Header";
+import Navbar from "./navbar/Navbar";
+
+export default function RouterPage() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+}
